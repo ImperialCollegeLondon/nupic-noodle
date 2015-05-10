b05b7ab53ec715b7b4fff848f28d273564e9c22f
M J Harvey <matt@mjharvey.org.uk>
2015-05-10 15:37:45 +0200

    Bug?

---
+++ test1/run-me.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from nupic.frameworks.opf.modelfactory import ModelFactory
+from nupic.swarming import permutations_runner
+from nupic_output import NuPICFileOutput, NuPICPlotOutput
+import shutil
+import math
+import  csv
+import json
+from numpy.random import random_integers
+
+
+#with open('searchDef.json') as df:
+#	swarm_config = json.load(df);
+
+#model_params = permutations_runner.runWithConfig( swarm_config, 
+#	{'maxWorkers':4, 'overwrite': True}, "reports/mjh", "." )
+
+model_params = permutations_runner.runWithPermutationsScript ( "permutations.py", 
+			{'maxWorkers':4, 'overwrite': True}, ".", "." )
+
+model = ModelFactory.create( model_params )
+
